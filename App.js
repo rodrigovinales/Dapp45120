0cf5485c2bcae5228a7c7ee1882550a1922db32a
Rodrigo Viñales <rodrigo11@live.com.ar>
2022-10-24 21:15:48 -0300

    Clase 2

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text1}>Hola, Coder!</Text>
+      <Text style={styles.text2}>Usando Expo conectando un celular fisico JM-500......</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +18,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text1: {
+    fontSize: 92,
+    color: '#9E7676',
+    textAlign: 'center'
+  },
+  text2: {
+    fontSize: 22,
+    color: '#CF0A0A',
+    marginTop:45,
+    textAlign: 'center'
+  }
 });
